e54d9053e13806800f862c80e0683079f91bab55
Nicolas Trierveiler <nicolastrierveiler@gmail.com>
2022-04-25 00:53:26 -0300

    Aula Finalizada

--- Functions/excercise.js
+++ Functions/excercise.js
@@ -14,10 +14,26 @@ return '${nome} ${sobrenome}';
 }
 
 // Crie uma função que verifica se um número é par
+function isEven(number){
+    var modulo = numero % 2;
+    if(modulo === 0) {
+        return true;
+    }
+        else {
+            return false
+        }
+    }
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+function dataDisplay(data) {
+    return typeof data;
+}
+
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'click' ocorrer.
+addEventListener('click', function() {
+    console.log('Nicolas Trierveiler')
+});
